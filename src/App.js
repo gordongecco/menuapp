8af21445e8ced8f01dea72557cb5585af7b272c2
gordongecco <delldani001@gmail.com>
2019-09-06 10:57:52 +0200

    09.06

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Menu1() {
+  return (
+    <div style={{ backgroundColor: 'red', width: 90 }} id="m1" draggable="true" onDragStart={drag}>
+      Menu 1
+    </div>
+  );
+}
+function Menu2() {
+  return (
+    <div style={{ backgroundColor: 'blue', width: 90 }} id="m2" draggable="true" onDragStart={drag}>
+      Menu 2
+    </div>
+  );
+}
+function Menu3() {
+  return (
+    <div
+      style={{ backgroundColor: 'green', width: 90 }}
+      id="m3"
+      draggable="true"
+      onDragStart={drag}
+    >
+      Menu 3
+    </div>
+  );
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData('text/plain', ev.target.id);
+}
+
+const drop = (m) => (ev) => {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData('text/plain');
+  console.log(data);
+  console.log(m);
+  ev.target.appendChild(document.getElementById(data));
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul style={{ listStyleType: 'none' }}>
+        <li id="l1" onDrop={drop} onDragOver={allowDrop}>
+          <Menu1 />
+        </li>
+        <li id="l2" onDrop={drop} onDragOver={allowDrop}>
+          <Menu2 />
+        </li>
+        <li id="l3" onDrop={drop} onDragOver={allowDrop}>
+          <Menu3 />
+        </li>
+      </ul>
+      <div id="div2" onDrop={drop('div2')} onDragOver={allowDrop}>
+        gfgfgfgfgfgf
+      </div>
     </div>
   );
 }
