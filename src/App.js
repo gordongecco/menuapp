2393e355a889ff37b65598e3b1fae90e7049fff0
gordongecco <delldani001@gmail.com>
2019-09-06 11:24:41 +0200

    09.06

--- src/App.js
+++ src/App.js
@@ -38,22 +38,22 @@ function drag(ev) {
 const drop = (m) => (ev) => {
   ev.preventDefault();
   var data = ev.dataTransfer.getData('text/plain');
-  console.log(data);
-  console.log(m);
-  ev.target.appendChild(document.getElementById(data));
+  console.log('data:' + data);
+  console.log('m: ' + m);
+  if (!(m == 'l1' && data == 'm1')) ev.target.appendChild(document.getElementById(data));
 };
 
 function App() {
   return (
     <div>
       <ul style={{ listStyleType: 'none' }}>
-        <li id="l1" onDrop={drop} onDragOver={allowDrop}>
+        <li id="l1" onDrop={drop('l1')} onDragOver={allowDrop}>
           <Menu1 />
         </li>
-        <li id="l2" onDrop={drop} onDragOver={allowDrop}>
+        <li id="l2" onDrop={drop('l2')} onDragOver={allowDrop}>
           <Menu2 />
         </li>
-        <li id="l3" onDrop={drop} onDragOver={allowDrop}>
+        <li id="l3" onDrop={drop('l3')} onDragOver={allowDrop}>
           <Menu3 />
         </li>
       </ul>
