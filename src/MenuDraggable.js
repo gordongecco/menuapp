946776bff744ce1483a3c1a355abf0f72c9cbc9e
gordongecco <delldani001@gmail.com>
2019-09-11 15:02:39 +0200

    09.11

--- src/MenuDraggable.js
+++ src/MenuDraggable.js
@@ -4,6 +4,9 @@ export default class MenuDraggable extends Component {
   constructor(props) {
     super(props);
 
+    this.ranId = Math.floor(Math.random() * 90000) + 10000;
+    console.log('constr: ' + this.ranId);
+
     this.state = {
       menuItemsArray: [
         { name: 'menu 1', color: 'blue' },
@@ -18,20 +21,24 @@ export default class MenuDraggable extends Component {
     ev.preventDefault();
   }
 
-  dragStart = (id) => (ev) => {
+  dragStart = (id, ranId) => (ev) => {
     ev.dataTransfer.setData('text/plain', id);
+    ev.dataTransfer.setData('draggableId', ranId);
   };
 
   drop = (target) => (ev) => {
     ev.preventDefault();
-    var menuItem = ev.dataTransfer.getData('text/plain');
-    const targetIndex = target;
-    const itemIndex = menuItem;
-    let array = this.state.menuItemsArray;
-    const temp = array[targetIndex];
-    array[targetIndex] = array[itemIndex];
-    array[itemIndex] = temp;
-    this.setState({ menuItemsArray: array });
+    let menuItem = ev.dataTransfer.getData('text/plain');
+    let draggableId = ev.dataTransfer.getData('draggableId');
+    if (this.ranId == draggableId) {
+      const targetIndex = target;
+      const itemIndex = menuItem;
+      let array = this.state.menuItemsArray;
+      const temp = array[targetIndex];
+      array[targetIndex] = array[itemIndex];
+      array[itemIndex] = temp;
+      this.setState({ menuItemsArray: array });
+    }
   };
 
   render() {
@@ -39,7 +46,7 @@ export default class MenuDraggable extends Component {
       return (
         <li
           draggable="true"
-          onDragStart={this.dragStart(index)}
+          onDragStart={this.dragStart(index, this.ranId)}
           style={{ backgroundColor: item.color, width: 90 }}
           onDrop={this.drop(index)}
           onDragOver={this.allowDrop}
