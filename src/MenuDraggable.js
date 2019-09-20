204db2c2c80b9a499ce86b1e6baecd3be3fdfc96
gordongecco <gordongecco77@gmail.com>
2019-09-20 17:10:41 +0200

    09.20

--- src/MenuDraggable.js
+++ src/MenuDraggable.js
@@ -10,6 +10,9 @@ export default class MenuDraggable extends Component {
       active: null,
       activeItemIndex: null,
     };
+
+    // console.log(this.props.menuItems);
+
   }
 
   allowDrop(ev) {
@@ -108,6 +111,9 @@ export default class MenuDraggable extends Component {
   }
 
   render() {
+    let array = this.state.menuItemsArray;
+    array.forEach(element => {element.flag = true
+    });
     const items = this.getItems(this.state.menuItemsArray, '', '');
     return (
       <div>
