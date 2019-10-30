d3aeccdb7f57b1acd3c230b2d2016dcd035f1480
gordongecco <delldani001@gmail.com>
2019-10-30 15:45:15 +0100

    10.30

--- test.js
+++ test.js
@@ -9,10 +9,28 @@ Enzyme.configure({ adapter: new Adapter() });
 const menu1 = [
   { name: 'menu 1', parent: '', color: 'blue' },
   { name: 'menu 2', parent: '', color: 'red' },
-  { name: 'menu 3', parent: 'menu 4', color: 'green' },
   { name: 'menu 4', parent: '', color: 'yellow' },
   { name: 'menu 5', parent: '', color: 'pink' },
+  { name: 'menu 6', parent: 'menu 4', color: 'pink' },
 ];
+const expectedMenu = [
+  { name: 'menu 1', parent: '', color: 'blue' },
+  { name: 'menu 2', parent: '', color: 'red' },
+  {
+    name: 'menu 4',
+    parent: '',
+    color: 'yellow',
+    children: [
+      {
+        color: 'pink',
+        name: 'menu 6',
+        parent: 'menu 4',
+      },
+    ],
+  },
+  { name: 'menu 5', parent: '', color: 'pink' },
+];
+
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
@@ -20,4 +38,9 @@ test('adds 1 + 2 to equal 3', () => {
 test('enzyme test shallow rendering', () => {
   const wrapper = shallow(<MenuDraggable menuItems={menu1} />);
   expect(wrapper.contains('sasa')).toBe(true);
+});
+
+test('enzyme test DOM rendering', () => {
+  const wrapper = shallow(<MenuDraggable menuItems={menu1} />);
+  expect(wrapper.instance().sortElements(menu1)).toEqual(expectedMenu);
 });
